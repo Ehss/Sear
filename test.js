a122afeb3306720894e58c907500f5daf83763af
Steven Jacks <32830428+Ehss@users.noreply.github.com>
2018-03-26 17:57:59 +0800

    Example of implementation of Library Commands

---
+++ test.js
@@ -0,0 +1,10 @@
+addLocation("MyBedroom");
+addLocation("Hallway");
+addLocation("SharedBathroom");
+addLocation("GuestBedroom");
+addLocation("BrothersBedroom");
+addLocation("MasterBedroom");
+addLocation("Stairs");
+addLocation("MasterBedroomBed");
+addLocation("MasterBedroomBath");
+

bc72419029c949dd6219b217cd730ac939250767
Steven Jacks <32830428+Ehss@users.noreply.github.com>
2018-04-18 18:35:02 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,54 @@
+//play all music at the start here
 
 playSound("theone.mp3");
 audios[0].loop = true;
 audios[0].pause();
 
-audioDistances.push(["MyBedroom",0,1]);
-audioDistances.push(["Hallway",0,0.5]);
-audioDistances.push(["Bathroom",0,0.2]);
-audioDistances.push(["GuestBedroom",0,0.2]);
+// add music distances for all rooms. 
+// (Non labeled rooms will not change from other rooms)
 
-addLocations(
+addLocations(["AptBedroom","AptKitchen","AptLivingRoom","AptBathroom"]);
+
+currentLocation = "AptBedroom";
+
+addStateDescripToLocation("AptBedroom", "Normal", "You're standing in your bedroom.<br><br>To the South you see the Bathroom. To the East is Living Room.");
+addStateDescripToLocation("AptKitchen", "Normal", "You're standing in the kitchen.<br><br>To the North you see the Living Room. To the West is the Bathroom.");
+addStateDescripToLocation("AptLivingRoom", "Normal", "You're standing in the living room.<br><br>To the South you see the Kitchen. To the West is Bedroom.");
+addStateDescripToLocation("AptBathroom", "Normal", "You're standing in the bathroom.<br><br>To the North you see the Bedroom. To the East is Kitchen.");
+
+addMove("AptBedroom", ["Normal"], "AptLivingRoom", ["Normal"], ["Go East","East","E","Move East"]);
+addMove("AptLivingRoom", ["Normal"], "AptBedroom", ["Normal"], ["Go West","West","W","Move West"]);
+
+addMove("AptBathroom", ["Normal"], "AptKitchen", ["Normal"], ["Go East","East","E","Move East"]);
+addMove("AptKitchen", ["Normal"], "AptBathroom", ["Normal"], ["Go West","West","W","Move West"]);
+
+addMove("AptBathroom", ["Normal"], "AptBedroom", ["Normal"], ["Go North","North","N","Move North"]);
+addMove("AptBedroom", ["Normal"], "AptBathroom", ["Normal"], ["Go South","South","S","Move South"]);
+
+addMove("AptKitchen", ["Normal"], "AptLivingRoom", ["Normal"], ["Go North","North","N","Move North"]);
+addMove("AptLivingRoom", ["Normal"], "AptKitchen", ["Normal"], ["Go South","South","S","Move South"]);
+
+var apple = new Item("Apple");
+apple.addState(new State("Normal","The apple is shiny and red. Looks good enough to eat!"));
+addItemToLocation(apple, "AptKitchen");
+
+/*addLocations(
   ["MyBedroom","Hallway","Bathroom","GuestBedroom",
    "MasterBedroom","Stairs","MasterBedroomBed","MasterBedroomBath"
   ]);
+*/
+
+/*
+audioDistances.push(["WakeUpRoom",0,1]);
+audioDistances.push(["Hallway",0,0.5]);
+audioDistances.push(["Bathroom",0,0.2]);
+audioDistances.push(["GuestBedroom",0,0.2]);
+*/
+
 
 //addStateDescripToLocation(location, state, description)
 
+/*
 addStateDescripToLocation("MyBedroom", "Normal", "You're standing in your bedroom. There's a bed, freshly made. A door west leads out from the room.");
 addStateDescripToLocation("Hallway", "Normal", "You're standing in the hallway. This connects everything - your bedroom to the east, a bathroom to the south-east, a guest bedroom to the south, the stairs to the north, and your parents' bedroom to the north west.");
 addStateDescripToLocation("Bathroom", "Normal", "You're standing in the bathroom. There's a sink, a shower, and a toilet. Not much else. The only way out is the hallway to the north west.");
@@ -22,6 +56,7 @@ addStateDescripToLocation("GuestBedroom", "Normal", "You're standing in the gues
 addStateDescripToLocation("Stairs", "Normal", "You're standing at the top of the stairs. The stairs run down, and the hallway is to the south.");
 addStateDescripToLocation("MasterBedroomBed", "Normal", "You're standing in the master bedroom. The master bath is to the west, and the hallway is to the south east.");
 addStateDescripToLocation("MasterBedroomBath", "Normal", "You're standing in the master bathroom. There're two sinks, a shower, and a toilet. The master bedroom is to the east.");
+
 
 addMove("MyBedroom", ["Normal"], "Hallway", ["Normal"], ["Go West","West","W"]);
 addMove("Hallway", ["Normal"], "MyBedroom", ["Normal"], ["Go East","East","E"]);
@@ -32,7 +67,7 @@ addMove("Bathroom", ["Normal"], "Hallway", ["Normal"], ["Go Northwest","Northwes
 addMove("Hallway", ["Normal"], "GuestBedroom", ["Normal"], ["Go South","South","s","Move South"]);
 addMove("GuestBedroom", ["Normal"], "Hallway", ["Normal"], ["Go North","North","n","Move North"]);
 
-currentLocation = "MyBedroom";
+
 
 addPicture("MyBedroom","dragonballs.png");
 addPicture("Hallway","dragonballs2.png");
@@ -40,8 +75,6 @@ addPicture("GuestBedroom","dragonball.png");
 
 var apple = new Item("Apple");
 apple.addState(new State("Normal","The apple is shiny and red. Looks good enough to eat!"));
-
-
 addItemToLocation(apple, "MyBedroom");
 
 
@@ -108,3 +141,5 @@ addPossibleAction(
 	"The music is paused."
 
 );
+
+*/
